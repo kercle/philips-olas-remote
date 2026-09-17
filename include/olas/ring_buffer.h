179847c6bb259e9f5ba4456176f677fe0a32cd6f
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-09-18 01:10:40 +0200

    Improve ring buffer implementation.

--- include/olas/ring_buffer.h
+++ include/olas/ring_buffer.h
@@ -1,22 +1,88 @@
 #pragma once
 
+#include <array>
 #include <atomic>
+#include <cstdint>
 #include <optional>
-#include <stdlib.h>
+#include <type_traits>
 
-// Single-producer, single-consumer ring buffer.
+namespace detail {
+
+#ifdef ESP8266
+
+class SpscIndex {
+public:
+    uint32_t IRAM_ATTR load_relaxed() const
+    {
+        return value;
+    }
+
+    uint32_t IRAM_ATTR load_acquire() const
+    {
+        const uint32_t result = value;
+
+        // Do not move subsequent memory accesses before this load.
+        std::atomic_signal_fence(std::memory_order_acquire);
+
+        return result;
+    }
+
+    void IRAM_ATTR store_release(uint32_t new_value)
+    {
+        // Do not move preceding memory accesses after publication.
+        std::atomic_signal_fence(std::memory_order_release);
+
+        value = new_value;
+    }
+
+private:
+    volatile uint32_t value = 0;
+};
+
+#else
+
+static_assert(
+    std::atomic<uint32_t>::is_always_lock_free,
+    "RingBuffer requires lock-free 32-bit atomics");
+
+class SpscIndex {
+public:
+    uint32_t IRAM_ATTR load_relaxed() const
+    {
+        return value.load(std::memory_order_relaxed);
+    }
+
+    uint32_t IRAM_ATTR load_acquire() const
+    {
+        return value.load(std::memory_order_acquire);
+    }
+
+    void IRAM_ATTR store_release(uint32_t new_value)
+    {
+        value.store(new_value, std::memory_order_release);
+    }
+
+private:
+    std::atomic<uint32_t> value { 0 };
+};
+
+#endif
+
+}
+
 template <typename T, size_t N>
 class RingBuffer {
     static_assert(N > 0);
     static_assert((N & (N - 1)) == 0);
+    static_assert(std::is_trivially_copyable_v<T>);
 
     static constexpr size_t mask = N - 1;
 
 public:
-    bool push(const T& value)
+    bool IRAM_ATTR push(const T& value)
     {
-        const uint32_t write = write_index.load(std::memory_order_relaxed);
-        const uint32_t read = read_index.load(std::memory_order_acquire);
+        const uint32_t write = write_index.load_relaxed();
+        const uint32_t read = read_index.load_acquire();
 
         if (write - read == N) {
             return false;
@@ -24,26 +90,23 @@ public:
 
         data[write & mask] = value;
 
-        write_index.store(
-            write + 1,
-            std::memory_order_release);
+        write_index.store_release(write + 1);
 
         return true;
     }
 
     std::optional<T> pop()
     {
-        const uint32_t read = read_index.load(std::memory_order_relaxed);
-        const uint32_t write = write_index.load(std::memory_order_acquire);
+        const uint32_t read = read_index.load_relaxed();
+        const uint32_t write = write_index.load_acquire();
 
         if (read == write) {
             return std::nullopt;
         }
 
-        const auto value = data[read & mask];
-        read_index.store(
-            read + 1,
-            std::memory_order_release);
+        const T value = data[read & mask];
+
+        read_index.store_release(read + 1);
 
         return value;
     }
@@ -51,6 +114,6 @@ public:
 private:
     std::array<T, N> data { };
 
-    std::atomic<uint32_t> write_index = 0;
-    std::atomic<uint32_t> read_index = 0;
+    detail::SpscIndex write_index;
+    detail::SpscIndex read_index;
 };
