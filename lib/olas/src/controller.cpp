b411276d9e3ee91f7ec02f977a0939b387a850b5
Clemens Kerschbaumer <clemens.kerschbaumer@gmail.com>
2026-09-22 00:48:19 +0200

    Fix: Infinite loop when foreign ID is received.

--- lib/olas/src/controller.cpp
+++ lib/olas/src/controller.cpp
@@ -81,6 +81,7 @@ void Controller::handle_received_data()
             // Not our device.
             // In the future, we can record these
             // IDs for runtime pairing.
+            frame = transmitter.receive_frame();
             continue;
         }
 
